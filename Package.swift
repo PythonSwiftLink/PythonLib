8b95f925a8ad93fd3e764486b62d138b2554bc1c
psychowaspx <psychowaspx@gmail.com>
2022-11-03 15:34:14 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,41 +3,38 @@
 import PackageDescription
 
 let framework_path = "Frameworks310"
-
-
+var package_targets = [Target]()
+package_targets.append(
+    .target(
+        name: "PythonLib",
+        dependencies: ["Python"],
+        linkerSettings: [
+            .linkedLibrary("ncurses"),
+            .linkedLibrary("ffi"),
+            .linkedLibrary("sqlite3"),
+            .linkedLibrary("z"),
+            .linkedLibrary("panel"),
+            
+        ]
+    )
+)
 #if os(macOS)
-
-let package = Package(
-    name: "PythonLib",
-    products: [
-        .library(
-            name: "PythonLib",
-            targets: ["PythonLib"]
-        ),
-    ],
-    targets: [
-        .target(
-            name: "PythonLib",
-            dependencies: ["Python"],
-            linkerSettings: [
-                .linkedLibrary("ncurses"),
-                .linkedLibrary("ffi"),
-                .linkedLibrary("sqlite3"),
-                .linkedLibrary("z"),
-                .linkedLibrary("panel"),
-                
-            ]
-        ),
-        .binaryTarget(
-            name: "Python",
-            url: "https://github.com/PythonSwiftLink/PythonAppleSupport/archive/refs/tags/3.10-macOS.b6.zip",
-            checksum: "a5e256b619be86b10b93118e6a277f3a9e98dc7eaf57d3059c56fdbd074a0314"
-        ),
-    ]
+package_targets.append(
+    .binaryTarget(
+        name: "Python",
+        url: "https://github.com/PythonSwiftLink/PythonAppleSupport/archive/refs/tags/3.10-macOS.b6.zip",
+        checksum: "a5e256b619be86b10b93118e6a277f3a9e98dc7eaf57d3059c56fdbd074a0314"
+    )
 )
-
 #else
-
+package_targets.append(
+    .binaryTarget(
+        name: "Python",
+        url: "https://github.com/PythonSwiftLink/PythonAppleSupport/archive/refs/tags/3.10-iOS.b6.zip",
+        checksum: "a5e256b619be86b10b93118e6a277f3a9e98dc7eaf57d3059c56fdbd074a0314"
+    )
+)
+#endif
 let package = Package(
     name: "PythonLib",
     products: [
@@ -46,27 +43,38 @@ let package = Package(
             targets: ["PythonLib"]
         ),
     ],
-    targets: [
-        .target(
-            name: "PythonLib",
-            dependencies: ["Python"],
-            linkerSettings: [
-                .linkedLibrary("ncurses"),
-                .linkedLibrary("ffi"),
-                .linkedLibrary("sqlite3"),
-                .linkedLibrary("z"),
-                .linkedLibrary("panel"),
-                
-            ]
-        ),
-        .binaryTarget(
-            name: "Python",
-            url: "https://github.com/PythonSwiftLink/PythonAppleSupport/archive/refs/tags/3.10-iOS.b6.zip",
-            checksum: "a5e256b619be86b10b93118e6a277f3a9e98dc7eaf57d3059c56fdbd074a0314"
-        ),
-    ]
+    targets: package_targets
 )
 
-#endif
+
+//let package = Package(
+//    name: "PythonLib",
+//    products: [
+//        .library(
+//            name: "PythonLib",
+//            targets: ["PythonLib"]
+//        ),
+//    ],
+//    targets: [
+//        .target(
+//            name: "PythonLib",
+//            dependencies: ["Python"],
+//            linkerSettings: [
+//                .linkedLibrary("ncurses"),
+//                .linkedLibrary("ffi"),
+//                .linkedLibrary("sqlite3"),
+//                .linkedLibrary("z"),
+//                .linkedLibrary("panel"),
+//
+//            ]
+//        ),
+//        .binaryTarget(
+//            name: "Python",
+//            url: "https://github.com/PythonSwiftLink/PythonAppleSupport/archive/refs/tags/3.10-iOS.b6.zip",
+//            checksum: "a5e256b619be86b10b93118e6a277f3a9e98dc7eaf57d3059c56fdbd074a0314"
+//        ),
+//    ]
+//)
+
 
 
